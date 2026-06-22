fc9aadbfdcfada42b4c81593be360e4ae0ed269d
Vikas Kasaudhan <kasaudhanv036@gmail.com>
2026-06-22 13:45:25 +0530

    Fix SQL syntax for user table creation

--- backend/models/user_model.py
+++ backend/models/user_model.py
@@ -1,17 +1,21 @@
 def create_user_table(conn):
+
     cursor = conn.cursor()
 
     cursor.execute("""
-    CREATE TABLE IF NOT EXISTS users(
-<<<<<<< HEAD
-    id SERIAL PRIMARY KEY ,
-=======
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
->>>>>>> 61108bc051646086413a5603c7e795890ca47c7e
-    name TEXT NOT NULL,
-    email TEXT UNIQUE NOT NULL,
-    password TEXT NOT NULL
-    ,created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP)
-""")
+        CREATE TABLE IF NOT EXISTS users(
+
+            id SERIAL PRIMARY KEY,
+
+            name TEXT NOT NULL,
+
+            email TEXT UNIQUE NOT NULL,
+
+            password TEXT NOT NULL,
+
+            created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+
+        )
+    """)
 
     conn.commit()
