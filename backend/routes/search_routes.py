8dc476e84f46b882ea66ada2f307377d699a37b7
Vikas Kasaudhan <kasaudhanv036@gmail.com>
2026-06-22 13:31:47 +0530

    Refactor search query and import path

--- backend/routes/search_routes.py
+++ backend/routes/search_routes.py
@@ -2,16 +2,13 @@ from flask import Blueprint
 from flask import request
 from flask import jsonify
 
-<<<<<<< HEAD
-from Debtbook.backend.config.database import get_connection
-=======
 from config.database import get_connection
->>>>>>> 61108bc051646086413a5603c7e795890ca47c7e
 
 search_bp = Blueprint(
     "search",
     __name__
 )
+
 
 @search_bp.route("/search")
 def search():
@@ -26,25 +23,27 @@ def search():
 
     cursor.execute(
         """
-        SELECT id,name,phone
+        SELECT
+            id,
+            name,
+            phone
         FROM people
-        WHERE name LIKE ?
+        WHERE name ILIKE %s
         """,
         (f"%{query}%",)
     )
 
     rows = cursor.fetchall()
 
+    conn.close()
+
     result = []
 
     for row in rows:
-
         result.append({
             "id": row[0],
             "name": row[1],
             "phone": row[2]
         })
-
-    conn.close()
 
     return jsonify(result)
