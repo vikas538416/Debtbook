33b29d72a42107f7e34b832411f223124993b69d
Vikas Kasaudhan <kasaudhanv036@gmail.com>
2026-06-22 13:31:07 +0530

    Refactor report_routes.py for import and query syntax

--- backend/routes/report_routes.py
+++ backend/routes/report_routes.py
@@ -1,43 +1,42 @@
 from flask import Blueprint
 from flask import jsonify
 
-<<<<<<< HEAD
-from Debtbook.backend.config.database import get_connection
-=======
 from config.database import get_connection
->>>>>>> 61108bc051646086413a5603c7e795890ca47c7e
 
 report_bp = Blueprint(
-    "report", __name__)
+    "report",
+    __name__
+)
+
+
 @report_bp.route(
-<<<<<<< HEAD
-    "/report/<int:person_id>",methods=['GET']
-=======
-    "/report/<int:person_id>"
->>>>>>> 61108bc051646086413a5603c7e795890ca47c7e
+    "/report/<int:person_id>",
+    methods=["GET"]
 )
 def report(person_id):
+
     conn = get_connection()
     cursor = conn.cursor()
+
     cursor.execute(
         """
         SELECT
-        amount,
-        type,
-        note,
-        created_at
+            amount,
+            type,
+            note,
+            created_at
         FROM transactions
-<<<<<<< HEAD
-        WHERE person_id=%s
-=======
-        WHERE person_id=?
->>>>>>> 61108bc051646086413a5603c7e795890ca47c7e
+        WHERE person_id = %s
         """,
         (person_id,)
     )
+
     rows = cursor.fetchall()
+
     conn.close()
+
     result = []
+
     for row in rows:
         result.append({
             "amount": row[0],
@@ -45,4 +44,5 @@ def report(person_id):
             "note": row[2],
             "created_at": row[3]
         })
+
     return jsonify(result)
