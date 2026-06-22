9e3150406269aaa0cfd7192c0a0347fa8e993c14
vikas <vikask786253@gmail.com>
2026-06-22 09:15:50 +0530

    modified app.py

--- backend/config/database.py
+++ backend/config/database.py
@@ -1,20 +1,9 @@
-<<<<<<< HEAD
 import psycopg2
 from dotenv import load_dotenv
 import os
 
 
 load_dotenv()
-
-# DATABASE_PATH = "database/debtbook.db"
-
-# def get_connection():
-
-#     os.makedirs(
-#         "database",exist_ok=True
-#     )
-
-#     return sqlite3.connect(DATABASE_PATH)
 
 
 def get_connection():
@@ -25,19 +14,3 @@ def get_connection():
         password = os.getenv("DB_PASSWORD"),
         port = os.getenv("DB_PORT")
     )
-=======
-import sqlite3
-import os
-
-DATABASE_PATH = "database/debtbook.db"
-
-def get_connection():
-
-    os.makedirs(
-        "database",exist_ok=True
-    )
-
-    return sqlite3.connect(DATABASE_PATH)
-
-
->>>>>>> 61108bc051646086413a5603c7e795890ca47c7e
